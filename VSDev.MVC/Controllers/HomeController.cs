020607cc8260486bdd081f2ff8f16e59928c3073
Victor Savoi <savoi.victor@hotmail.com>
2020-05-18 11:22:54 -0300

    First commit

---
+++ VSDev.MVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSDev.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
