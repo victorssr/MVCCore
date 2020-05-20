d558df6c78e2531e140a038c0873de73fd5af8aa
Victor Savoi <savoi.victor@hotmail.com>
2020-05-20 11:03:10 -0300

    Using Razor extensions to control the views with authorization

---
+++ VSDev.MVC/Extensions/RazorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace VSDev.MVC.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static bool IfClaim(this RazorPage page, string claimType, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimType, claimValue);
+        }
+
+        public static string IfClaimShow(this RazorPage page, string claimType, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimType, claimValue) ? "" : "disabled";
+        }
+
+        public static IHtmlContent IfClaimShow(this IHtmlContent page, HttpContext context, string claimType, string claimValue)
+        {
+            return CustomAuthorization.ValidarClaimsUsuario(context, claimType, claimValue) ? page : null;
+        }
+    }
+}
